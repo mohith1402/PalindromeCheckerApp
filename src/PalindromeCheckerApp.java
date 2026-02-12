14f482e9e1ae963a9036d011304749017bf2030e
mohith1402 <ballolamohith1402@gmail.com>
2026-02-12 16:06:41 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+/*
+* UC1 Palindrome Checker
+* -Printing welcome message
+* -showing application version
+*
+* No pelindrome logic is implemented yet.
+*
+* @author Mohith
+* @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Managment System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized Successfully");
+    }
+}
